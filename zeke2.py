1b399333f1bdb974c7436c9f5347ad155a81fafc
Laurie <bluepeppers@archlinux.us>
2010-01-07 18:29:32 +0000

    Added superclass

--- zeke2.py
+++ zeke2.py
@@ -5,107 +5,24 @@ import anydbm
 global db
 db = anydbm.open('/home/laurie/PersonalMedia/Code/Python/Zeke/Maintrunk/words', 'c')
 
-class word(object):
-	def __init__(self, word=None):
-		#for iters
-		self.place = -1
-		
-		self.word = word
-		#asses = list of words
-		self.asses = []
-	
-	def __add__(self, other):
-		if other.word in self.asses:
-			return None
-		self.asses.append(other.word)
-		other.asses.append(self.word)
-	
-#	def __setattr__(self, attr, value):
-#		raise Forbidden(attr)
-	
-	def __eq__(self, other):
-		if not self.word.lower() == other.word.lower():
-			return False
-		else: return True
-	
-	def __str__(self):
-		return self.word
-	
-	def __getitem__(self, index):
-		return self.asses[index]
-	
-	def __iter__(self):
-		return self
-	
-	def next(self):
-		if self.place == len(self.asses) - 1:
-			self.place = -1
-			raise StopIteration
-		self.place += 1
-		return self.asses[self.place]
-	
-	def settype(self, type):
-		if not self.type:
-			raise TypeDefined(self.name, self.type)
-		self.type = type
-		type.words.append(self.name)
-	
-	def setstatus(self, status):
-		if not self.status:
-			raise StatusDefined(self.name, self.type)
-		self.status = status
-		status.words.append(self.name)
-	
-	def setuse(self, use):
-		if not self.use:
-			raise UseDefined(self.name, self.type)
-		self.use = use
-		use.words.append(self.name)
-	
-#	It would be LOVELY if this worked :(
-#	def __del__(self):
-#		db[self.name] = self
-
-
-class type(object):
-	def __init__(self, type=None):
-		self.type = type
+class Super(object):
+	def __init__(self, data=None):
+		self.data = data
 		
 		self.place = -1
 		
 		self.words = []
 	
-	def __add__(self, other):
-		if other.type:
-			raise TypeDefined(other.name, other.type)
-		else:
-			self.words.append(other.name)
-			other.type = self.type
+	def __eq__(self, other):
+		if not self.data.lower() == other.data.lower():
+			return False
+		else: return True
 	
-	def __floordiv__(self, other):
-		if type(other) == type(status()):
-			ret = []
-			for x in words:
-				#get the word from db, we only have name. so aslong as they're stored by name, it'll be cool
-				x = db[x]
-				if x.status == other: ret.append(x.name)
-				del x
-			return ret
-		
-		if type(other) == type(use()):
-			ret = []
-			for x in words:
-				#get the word from db, we only have name. so aslong as they're stored by name, it'll be cool
-				x = db[x]
-				if x.use == other: ret.append(x.name)
-				del x
-			return ret
-	
-	def __str__(self):
-		return self.type
+	def __repr__(self):
+		return self.data
 	
 	def __getitem__(self, index):
-		return self.type[index]
+		return self.words[index]
 	
 	def __iter__(self):
 		return self
@@ -115,9 +32,73 @@ class type(object):
 			self.place = -1
 			raise StopIteration
 		self.place += 1
-		return self.asses[self.place]
+		return self.words[self.place]
+	
+	def _StatusDiv(self, other):
+		if type(other) == type(Status()):
+			ret = []
+			for x in words:
+				#get the word from db, we only have name. so aslong as they're stored by name, it'll be cool
+				x = db[x]
+				if x.status == other: ret.append(x.data)
+				del x
+			return ret
+	def _UseDiv(self, other):
+		if type(other) == type(Use()):
+			ret = []
+			for x in words:
+				#get the word from db, we only have name. so aslong as they're stored by name, it'll be cool
+				x = db[x]
+				if x.use == other: ret.append(x.data)
+				del x
+			return ret
+	def _TypeDiv(self, other): pass
+		
 
-class use(object): pass
+class Word(Super):
+	def __init__(self, word=None):
+		#for iters
+		self.place = -1
+		
+		self.data = word
+		
+		#asses = list of words
+		self.asses = []
+	
+	def __add__(self, other):
+		if type(other) == type(Word()):
+			if other.data in self.asses:
+				return None
+			self.asses.append(other.data)
+			other.asses.append(self.data)
+		elif type(other) == type(Type()):
+			if other.type:
+				raise TypeDefined(other.data, other.type)
+			else:
+				self.words.append(other.data)
+				other.type = self.type
+		elif type(other) == type(Use()):
+			if other.use:
+				raise UseDefined(other.data, other.use)
+			else:
+				self.words.append(other.data)
+				other.use = self.use
+		elif type(other) == type(Status()):
+			if other.data:
+				raise StatusDefined(other.data, other.data)
+			else:
+				self.words.append(other.data)
+				other.data = self.use
+
+
+class Type(Super):
+	def __div__(self, other):
+		a = Super._StatusDiv(self, other)
+		if not a:
+			a = Super._UseDiv(self, other)
+		return a
+
+class Use(Super): pass
 
 class OwnErrors(Exception): pass
 
